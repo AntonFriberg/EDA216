03e69878ec9cae777769a3dc0bfcf653161169be
Oscar Gunnesson <ama10ogu@student.lu.se>
2016-03-23 12:15:07 +0100

    fixade till i db.sql

--- factorydb.sql
+++ factorydb.sql
@@ -1,25 +1,31 @@
 set FOREIGN_KEY_CHECKS = 0;
 
-drop table if exists Name;
+drop table if exists bill;
+drop table if exists Ingredient;
+drop table if exists Cookie;
+drop table if exists Recipe;
+drop table if exists Pallet;
+drop table if exists BillSpec;
+drop table if exists Customer;
 
 
 set FOREIGN_KEY_CHECKS = 1;
 
-create table Ingredient {
+create table Ingredient (
   ingrName varchar(40) not null,
   saldo integer not null CHECK (saldo>0),
   lastDel date not null,
   delAmount integer not null CHECK (delAmount>0),
 
   primary key (ingrName)
-  };
+  );
 
-create table cookie {
+create table Cookie (
   cookieName varchar(40) not null,
   primary key(cookieName)
-  };
+  );
 
-create table Recipie{
+create table Recipe(
   ingrName varchar(40) not null,
   cookieName varchar(40) not null,
   qty integer not null,
@@ -27,17 +33,17 @@ create table Recipie{
   primary key(ingrName,cookieName),
   foreign key(ingrName) references Ingredient(ingrName),
   foreign key(cookieName) references cookie(cookieName)
-  };
+  );
 
-create table Customer{
+create table Customer(
   custName varchar(40) not null,
   custAddr varchar(40) not null,
 
   primary key(custName,custAddr)
-  };
+  );
 
-create table Bill{
-  billId varchar(40) not null,
+create table Bill(
+  billId integer not null,
   delDate date not null,
   isDel char(0) default null,
   custName varchar(40) not null,
@@ -45,9 +51,9 @@ create table Bill{
 
   primary key(billId),
   foreign key(custName,custAddr) references Customer(custName,custAddr)
-  };
+  );
 
-create table Pallet{
+create table Pallet(
   palletNbr integer not null,
   cookieName varchar(40) not null,
   bakeDate date not null,
@@ -58,25 +64,25 @@ create table Pallet{
   primary key(palletNbr),
   foreign key(cookieName) references Cookie(cookieName),
   foreign key(billId) references Bill(billId)
-  };
+  );
 
-  create table BillSpec{
-    billId varchar(40) not null,
+  create table BillSpec(
+    billId integer not null,
     cookieName varchar(40) not null,
     nbrOfPallets integer not null,
 
     primary key(billId,cookieName),
     foreign key (billId) references Bill(billId),
     foreign key (cookieName) references Cookie(cookieName)
-    };
+    );
 
 
 
-  start transaction;
+  -- start transaction;
 
-  insert into TABLE values
+  -- insert into TABLE values
 
-  (Attr1,attr2),
-  (Attr3,Attr4);
+  -- (Attr1,attr2),
+  -- (Attr3,Attr4);
 
-  commit;
+ -- commit;
